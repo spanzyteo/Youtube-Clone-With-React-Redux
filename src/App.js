711d0aba7504f23e8a535d63e2899d9e88ab1d0b
spanzy teo <spanzytheo@gmail.com>
2023-09-18 16:20:20 +0100

    youtube_clone with react-redux and rapid api
    description

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Box } from '@mui/material'
+import {
+  Navbar,
+  Feed,
+  VideoDetail,
+  ChannelDetail,
+  SearchFeed,
+} from './components'
+
+const App = () => (
+  <BrowserRouter>
+    <Box sx={{ backgroundColor: '#000' }}>
+      <Navbar />
+      <Routes>
+        <Route path="/" exact element={<Feed />} />
+        <Route path="/video/:id" element={<videoDetail />} />
+        <Route path="/channel/:id" element={<channelDetail />} />
+        <Route path="/search/:searchTerm" element={<SearchFeed />} />
+      </Routes>
+    </Box>
+  </BrowserRouter>
+)
+
+export default App
