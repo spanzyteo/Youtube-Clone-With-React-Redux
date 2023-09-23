e3334d8c02a88e9869c8211eb830c9ad26b3bf9e
spanzy teo <spanzytheo@gmail.com>
2023-09-23 15:00:03 +0100

    changes made to my youtube clone
    description

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -8,23 +8,25 @@ import { Videos } from './'
 import { fetchFromAPI } from '../utils/fetchFromAPI'
 
 const VideoDetail = () => {
-  const [videoDetail, setVideoDetail] = useState()
+  const [videoDetail, setVideoDetail] = useState(null)
+  const [videos, setVideos] = useState(null)
   const { id } = useParams()
 
   useEffect(() => {
     fetchFromAPI(`videos?part=snippet,statistics&id=${id}`).then((data) =>
       setVideoDetail(data.items[0])
     )
+    fetchFromAPI(`search?part=snippet&relatedToVideoId=${id}&type=video`).then(
+      (data) => setVideos(data.items)
+    )
   }, [id])
+
+  if (!videoDetail?.snippet) return 'Loading...'
 
   const {
     snippet: { title, channelId, channelTitle },
     statistics: { viewCount, likeCount },
   } = videoDetail
-
-  if (!videoDetail?.snippet) {
-    return 'Loading...'
-  }
 
   return (
     <Box minHeight="95vh">
@@ -47,14 +49,37 @@ const VideoDetail = () => {
               px={2}
             >
               <Link to={`/channel/${channelId}`}>
-                <Typography variant={{ sm: 'subtitle1', md: 'h6' }}>
+                <Typography
+                  variant={{ sm: 'subtitle1', md: 'h6' }}
+                  color="#fff"
+                >
                   {channelTitle}
+                  <CheckCircle
+                    sx={{ fontSize: '12px', color: 'gray', ml: '5px' }}
+                  />
                 </Typography>
               </Link>
+              <Stack direction="row" gap="20px" alignItems="center">
+                <Typography variant="body1" sx={{ opacity: 0.7 }}>
+                  {parseInt(viewCount).toLocaleString()} views
+                </Typography>
+                <Typography variant="body1" sx={{ opacity: 0.7 }}>
+                  {parseInt(likeCount).toLocaleString()} likes
+                </Typography>
+              </Stack>
             </Stack>
           </Box>
         </Box>
       </Stack>
+
+      <Box
+        px={2}
+        py={{ md: 1, xs: 5 }}
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Videos videos={videos} />
+      </Box>
     </Box>
   )
 }
