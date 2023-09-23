9706db3178f79c60de811a24ea415f50053ef1d1
spanzy teo <spanzytheo@gmail.com>
2023-09-23 08:14:02 +0100

    changes made in youtube clone
    description

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const App = () => (
       <Navbar />
       <Routes>
         <Route path="/" exact element={<Feed />} />
-        <Route path="/video/:id" element={<videoDetail />} />
+        <Route path="/video/:id" element={<VideoDetail />} />
         <Route path="/channel/:id" element={<ChannelDetail />} />
         <Route path="/search/:searchTerm" element={<SearchFeed />} />
       </Routes>
